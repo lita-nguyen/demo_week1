24befd9d9a6034c804f56f7b89d6853207e8e65f
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-13 23:40:50 +0700

    operators

---
+++ ex3/ex3.js
@@ -0,0 +1,23 @@
+const a = 5, b = 10, c = 19;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(b / a);
+console.log(c % a);
+console.log(c ** a);
+
+let x = 5;
+const d = ++x; //d = x + 1 & x = x + 1
+console.log(" x = " + x)
+console.log(" d = " + d)
+
+let y = 5;
+const e = x++; // y = y + 1
+console.log(" y = " + y)
+console.log(" e = " + e)
+
+const u = 5; //number
+const v = "5"; //string
+console.log(u == v)
+console.log(u === v)
