01764e32bb97bd9e27de08663d753e02d65daef3
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-14 09:14:50 +0700

    console.log()

---
+++ ex4/ex4.js
@@ -0,0 +1,11 @@
+console.log("info");
+console.error("oops my mistake");
+console.warn("test warning");
+
+let name = "linh";
+let age = 21;
+//Dung dau phay tu dong them dau cach
+console.log("Name:",name,", Age:",age);
+
+//To mau console.log()
+console.log("%cWarning!","color:red;font-weight:bold;");
