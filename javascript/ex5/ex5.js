60449e38912d0ac4a66d14c982a7855e5f5ec0a9
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-14 09:28:54 +0700

    template string

---
+++ javascript/ex5/ex5.js
@@ -0,0 +1,9 @@
+const name = "linh";
+const age = 21;
+
+const introduction = "my name is " + name + "\nand I'm " + age;
+console.log(introduction)
+
+const intro = `my name is "nguyen le" 'nhat' ${name} 
+and I'm ${age}`;
+console.log(intro)
