3a920b92b5b6692c03f43539279cb644b9c73fb9
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-14 19:28:04 +0700

    function,arrow function

---
+++ javascript/ex9/ex9.js
@@ -0,0 +1,15 @@
+function greeting() {
+    console.log("hello");
+}
+greeting()
+
+function sum(a,b) {
+    return a + b;
+}
+console.log(sum(6,9));
+
+//arrow function
+const sum1 = (c,d) => {
+    return c + d;
+}
+console.log(sum1(10,9));
