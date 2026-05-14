4b27795e5b2d74b95f5729d646e7cb01d6f5a42a
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-14 18:40:09 +0700

    break,continue

--- javascript/ex8/ex8.js
+++ javascript/ex8/ex8.js
@@ -17,3 +17,19 @@ do {
     console.log("check score1: ",score1);
     score1--;
 } while (score1 > 50);
+
+//break
+for (let i = 1; i < 10; i++) {
+    console.log("i = ",i);
+    if (i === 5) {
+        break;
+    }
+}
+
+//continue
+for (let i = 1; i < 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log("i = ",i);
+}
