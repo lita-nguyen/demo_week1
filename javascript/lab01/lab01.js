fb90bce5f81f9dca054022be909a86df852042fd
Lita Nguyen <nguyenlenhatlinh512@gmail.com>
2026-05-14 09:46:22 +0700

    lab01

---
+++ javascript/lab01/lab01.js
@@ -0,0 +1,11 @@
+const fullName = "Nguyen Le Nhat Linh";
+
+const birthYear = 2005;
+const today = new Date();
+const currentYear = today.getFullYear();
+
+const isStudent = true;
+
+console.log(`Fullname: ${fullName},
+Age: ${currentYear - birthYear},
+Student: ${isStudent}`);
